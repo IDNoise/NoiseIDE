cb0dbcb5fc44dd1c138582a7d9efd17519ea85da
Yaroslav Nikityshev <YNikityshev@gmail.com>
2013-12-23 01:11:17 +0400

    inc wait interval

--- NoiseIDEPython/noiseide_copy.bat
+++ NoiseIDEPython/noiseide_copy.bat
@@ -1,7 +1,7 @@
 @rem BATCH FILE
 echo "Updating application. Do not close this window.
 @echo off
-ping -n 3 127.0.0.1 >nul 
+ping -n 10 127.0.0.1 >nul 
 
 @rem * Copy from source to destination including subdirs and hidden
 @rem * File
