9be8fc7b3622297bfaf1efdb54d8907247819358
Yaroslav Nikityshev <YNikityshev@gmail.com>
2013-12-23 11:56:12 +0400

    v 0.721 msi installer + shortcut + save install dir to registry

--- NoiseIDEPython/setup.py
+++ NoiseIDEPython/setup.py
@@ -9,8 +9,7 @@ if sys.platform == "win32":
     base = "Win32GUI"
 
 includefiles = ['dark.color.yaml', 
-                'white.color.yaml', 
-                'noiseide_copy.bat', 
+                'white.color.yaml',  
                 'rev.cfg', 
                 'noiseide.template.yaml', 
                 'data/images',
@@ -24,14 +23,43 @@ includefiles = ['dark.color.yaml',
     
 build_exe_options = {'include_files':includefiles} #, 'build_exe' : 'noiseide'
     
-company_name = "CoconutShavers"
 product_name = "NoiseIDE"
-    
+
+# Create a structure for the registry table
+# This will create a value 'InstallDir' in the key 'HKEY_LOCAL_MACHINE\SOFTWARE\MyCo\hello'
+registry_table = [('NoiseIDEKeyLM', 2, r'SOFTWARE\CoconutShavers\NoiseIDE', '*', None, 'TARGETDIR'),
+        ('NoiseIDEInstallDir', 2, r'SOFTWARE\CoconutShavers\NoiseIDE', 'InstallDir', '[TARGETDIR]', 'TARGETDIR'),]
+# A RegLocator table to find the install directory registry key when upgrading
+reg_locator_table = [('NoiseIDEInstallDirLocate', 2, r'SOFTWARE\CoconutShavers\NoiseIDE', 'InstallDir', 0)]
+# An AppSearch entry so that the MSI will search for previous installs
+# and update the default install location
+app_search_table = [('TARGETDIR', 'NoiseIDEInstallDirLocate')]
+
+shortcut_table = [
+    ("DesktopShortcut",        # Shortcut
+     "DesktopFolder",          # Directory_
+     "Noise IDE",              # Name
+     "TARGETDIR",              # Component_
+     "[TARGETDIR]NoiseIDE.exe",# Target
+     None,                     # Arguments
+     None,                     # Description
+     None,                     # Hotkey
+     None, # Icon
+     None,                     # IconIndex
+     None,                     # ShowCmd
+     'TARGETDIR'               # WkDir
+     )
+    ]
+
+
+# Now create the table dictionary
+msi_data = {'Registry': registry_table, 'RegLocator': reg_locator_table, 'AppSearch': app_search_table, "Shortcut": shortcut_table}
+
 bdist_msi_options = {
     'upgrade_code': '{ac7c7151-0f46-4061-a3b8-6321940165c0}',
     'add_to_path': False,
-    'initial_target_dir': r'C:\Programming\%s\%s' % (company_name, product_name)#,
-    #'build_exe' : 'noiseide'
+    'initial_target_dir': r'C:\Programming\%s' % (product_name),
+    'data': msi_data
     }
 
 def current_version():
